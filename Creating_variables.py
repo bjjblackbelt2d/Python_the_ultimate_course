6dd27489662d71e091f5c44ca10353dfe745313a
Seth Sacco <sethrsacco@hotmail.com>
2023-08-10 19:32:15 -0400

    random_num_gen

--- Creating_variables.py
+++ Creating_variables.py
@@ -7,3 +7,35 @@ c = "MyString"
 y= a + b
 print(y)
 print(c)
+
+## user input
+
+Name = input('Enter Your Name:')
+Age = input("Enter Your Age:")
+
+print("My Name is:", Name)
+print("My Age is:", Age)
+
+## math operators
+
+a = 100
+b = 20
+
+z = a + b 
+print("z value is", z)
+
+z = a - b
+print("z value is", z)
+
+z = a/b
+print("z value is", z)
+
+z = a * b
+print("z value is", z)
+
+## random number generator
+
+import random
+
+x =random.randint(0,2)
+print("random number is:", x)
