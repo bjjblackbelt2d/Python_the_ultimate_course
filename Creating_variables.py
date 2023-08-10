86fc4b21669f972a832257ed445dc20449a25341
Seth Sacco <sethrsacco@hotmail.com>
2023-08-10 19:18:52 -0400

    user_input

---
+++ Creating_variables.py
@@ -0,0 +1,9 @@
+## string manipulation
+
+a = 1
+b = 2.5
+c = "MyString"
+
+y= a + b
+print(y)
+print(c)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+Name = input('Enter Your Name:')
+Age = input("Enter Your Age:")
+
+print("My Name is:", Name)
+print("My Age is:", Age)
