084b2e8e8097b0227851b84f1a7393202f77a76d
Seth Sacco <sethrsacco@hotmail.com>
2023-08-11 20:18:22 -0400

    else_if_elif

--- Creating_variables.py
+++ Creating_variables.py
@@ -56,3 +56,65 @@ Name3RandomNumber = random.randint(0, 10)
 print(Name1, "Got a value of:", Name1RandomNumber)
 print(Name2, "Got a valule of:", Name2RandomNumber)
 print(Name3, "Got a value of:", Name3RandomNumber)
+
+## if statements
+
+skillpoints = 15
+
+if skillpoints < 15:
+    print("Welcome to city 1. THis is the easy part.")
+    
+  ## if elif else
+  
+forwardButton = True
+  
+backwardButton = True 
+
+#0 : no rotation
+#1 : clockwise rotation FW
+#2 : anti-clockwise rotation BW
+
+motorstatus = 0
+
+if forwardButton == True:
+    motorStatus = 1
+    print("Car is moving forward")
+
+elif backwardButton == True:
+    motorstatus = 2
+    print("Car is moving Backward")
+    
+else:
+    motorstatus = 0
+    print("Car is not moving")
+    
+## project grade
+
+grade = int(input("Enter the Grade: "))
+
+if grade>90:
+    print("You Got an AA! Congrats!")
+
+elif grade>85:
+    print("You Got an BA! Congrats!")
+
+elif grade>80:
+    print("You Got an BB! Congrats!")
+
+elif grade>75:
+    print("You Got an CB! Congrats!") 
+    
+elif grade>70:
+    print("You Got an CC! Congrats!") 
+    
+elif grade>65:
+    print("You Got an CD! Congrats!")
+    
+elif grade>60:
+    print("You Got an DC!")
+    
+elif grade>55:
+    print("You Got an F! You suck!")
+  
+  
+  
