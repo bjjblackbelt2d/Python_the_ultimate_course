90e6ce647f78406a8c19fc0de7daafbb1e656d6e
Seth Sacco <sethrsacco@hotmail.com>
2023-08-26 18:29:17 -0400

    _cliapp.py

--- Creating_variables.py
+++ Creating_variables.py
@@ -957,6 +957,14 @@ from zipfile import ZipFile
 with ZipFile('ZippedFiles.zip', 'w') as zip:
     zip.write("New Folder")
     
-    
-    
-    
+ 
+import argparse 
+
+my_parser = argparse.ArgumentParser (prog="app")
+
+my_parser.add_addargument('arg1', help='dummuy arg 1')
+my_parser.add_argument('arg2', help='dummuy arg 2')
+
+args = my_parser.parse_args()
+print("done executing")
+
